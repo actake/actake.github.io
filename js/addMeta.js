051f8e936aa2bb9dd7f463a218cff3ed96f11dc7
actake <960550591@qq.com>
2021-01-01 20:32:26 +0800

    Site updated: 2021-01-01 20:32:25

---
+++ js/addMeta.js
@@ -0,0 +1,8 @@
+;(function () {
+  // <meta name="google-site-verification" content="VkvnbEA39tPUuDz5C-X_b9RjJPpwsw4uJDNp2LcX_w8" />
+  var newMeta = document.createElement('meta');
+  newMeta.charset = 'utf-8';
+  newMeta.name = "google-site-verification";
+  newMeta.content = "VkvnbEA39tPUuDz5C-X_b9RjJPpwsw4uJDNp2LcX_w8";
+  document.getElementsByTagName('head')[0].appendChild(newMeta)
+})();
